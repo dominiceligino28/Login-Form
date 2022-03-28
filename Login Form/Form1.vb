cfefc3ca598d4561b593b4756b10f2248ec9c16f
Dominic Mina <Dominic Mina@DESKTOP-U7BDQGT>
2022-03-28 20:12:29 +0800

    Add project files.

---
+++ Login Form/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnLogIn_Click(sender As Object, e As EventArgs) Handles btnLogIn.Click
+        Try
+            logIn(txtUserName.Text.Trim, txtPW.Text.Trim)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+    Public Sub logIn(UserName As String, Password As String)
+        Try
+            Dim dt As New DataTable
+            Dim P As String
+
+            dt = ExecuteQuery("Select * FROM Table1 WHERE UserName = '" & txtUserName.Text.Trim & "'")
+            If dt.Rows.Count > 0 Then
+                P = dt.Rows(0)("Position")
+                If dt.Rows(0)("Password") = Password Then
+                    If P = "MANAGER" Then
+                        MessageBox.Show("Log In Successful As Manager", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    ElseIf P = "EMPLOYEE" Then
+                        MessageBox.Show("Log In Successful As Employee", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                Else
+                    MessageBox.Show("Invalid Username/Password", "Login failed", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Else
+                MessageBox.Show("Unregistered/Inactive account", "Login failed", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtUserName.TextChanged
+
+    End Sub
+End Class
---
+++ Login Form/mdlConnection.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Module mdlConnection
+
+    Public connStr As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Dominic Mina\Downloads\Database1.mdb"
+    Public conn As OleDbConnection = New OleDbConnection(connStr)
+        Public sql As String
+        Public dt As New DataTable
+
+    Function ExecuteQuery(ByVal Query As String) As DataTable
+
+        Dim sqlDT As New DataTable
+        Dim sqlCon As New OleDbConnection(connStr)
+        Dim sqlDA As New OleDbDataAdapter(Query, sqlCon)
+        Dim sqlCB As New OleDbCommandBuilder(sqlDA)
+        sqlDA.Fill(sqlDT)
+        Return sqlDT
+    End Function
+End Module
